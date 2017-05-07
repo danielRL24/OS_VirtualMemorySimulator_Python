4ff9a017df3a6dc2a31c2036e4f1ada2ec0f0c02
Daniel Rodrigues Lourenço <daniel.24.rodrigues@gmail.com>
2017-05-07 12:01:31 +0200

    Display a code. Prepare the process step by step of line.

--- tutoPygame.py
+++ tutoPygame.py
@@ -54,7 +54,6 @@ def display_interface(pygame, window, font):
 
     # Preparation
     memory = []
-    code = []
 
     for i in range(0, 10):
         type = TypeMemory.STACK
@@ -66,11 +65,6 @@ def display_interface(pygame, window, font):
             type = TypeMemory.HEAP_ALLOCATE
         memory.append(MemoryCase(pygame.Rect(500, 30 + i * 50, 200, 50), colors[type.value], type))
 
-    # Pointeurs
-    stack_ptr = 0
-    heap_ptr = 0
-    code_ptr = 0
-
     # Affichage initial
     window.fill((44, 62, 80, 255))
 
@@ -79,52 +73,90 @@ def display_interface(pygame, window, font):
         m.draw(window)
         i += 1
 
-    i = 0
-    for c in code:
-        c.display_text(window, font, i)
-        i += 1
-
-    # Display Code Area
-    code_area = pygame.Rect(50, 30, 300, 500)
+    # Affichage du rectangle contenant le code
+    code_area = pygame.Rect(50, 30, 350, 500)
     window.fill(Color(236, 240, 241, 255), code_area)
     pygame.draw.rect(window, (189, 195, 199, 255), code_area, 3)
 
+    # Quelques textes
     TextCode("Permanent Storage Area", TypeMemory.HEAP_ALLOCATE).display_text(window, font, 501, 495)
     TextCode("* Stack Area", TypeMemory.STACK, colors[TypeMemory.STACK.value]).display_text(window, font, 500, 535)
     TextCode("* Heap Area", TypeMemory.STACK, colors[TypeMemory.HEAP.value]).display_text(window, font, 500, 555)
     TextCode("CODE", None, Color(236, 240, 241, 255)).display_text(window, font, 50, 10)
     TextCode("MEMORY", None, Color(236, 240, 241, 255)).display_text(window, font, 500, 10)
+    TextCode("Press ESPACE to execute one line", None, Color(236, 240, 241, 255)).display_text(window, font, 50, 535)
 
+    i = 0
+    for c in CODE:
+        c.display_text(window, font, 85, 85 + i * 45)
+        i += 1
 
     pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
 
+
+# ------------------------------------------------------------
+# Action de la Touche Espace
+# ------------------------------------------------------------
+def step(pygame, window):
+    global CODE_POINT
+    global CODE_PTR
+    global STACK_PTR
+    global HEAP_STR
+    CODE_POINT.move_ip(0, 45)
+    pygame.draw.rect(window, Color(231, 76, 60, 255), CODE_POINT, 0)
+    CODE_PTR += 1
 
 # ------------------------------------------------------------
 # MAIN
 # ------------------------------------------------------------
 def main():
+    global CODE_POINT
+    global CODE_PTR
     pygame.init()
+    pygame.display.set_caption('Memory Simulator')
     # Ouverture de la fenêtre Pygame
     window = pygame.display.set_mode((800, 600))
     font = pygame.font.SysFont("monospace", 15)
 
     display_interface(pygame, window, font)
 
+    CODE_POINT = pygame.Rect(60, 85, 15, 15)
+    window.fill(Color(231, 76, 60, 255), CODE_POINT)
+
+    CODE_PTR = 1
+
     # Boucle d'animation
     animation = True
     while animation:
+        pygame.display.update()
         for event in pygame.event.get():
             if event.type == KEYDOWN:
-                if event.key == K_SPACE:
-                    print("Espace")
+                if event.key == K_SPACE and CODE_PTR < len(CODE):
+                    step(pygame, window)
             if event.type == QUIT:
                 animation = False
 
+        pygame.display.update()
+
+# ------------------------------------------------------------
+# Variables globales
+# ------------------------------------------------------------
+# Code à afficher
+CODE = [TextCode('int i = 10;', TypeMemory.STACK),
+        TextCode('User user = new User();', TypeMemory.HEAP),
+        TextCode('i = 25 + 6;', TypeMemory.STACK),
+        TextCode('delete user;', TypeMemory.HEAP),
+        TextCode('char[] c = malloc(2*sizeof(char));', TypeMemory.HEAP),
+        TextCode('strcpy(c, "LE");', TypeMemory.HEAP),
+        TextCode('free(c);', TypeMemory.HEAP)]
+
+# Pointeurs
+STACK_PTR = 0
+HEAP_STR = 0
+CODE_PTR = 0
+
+# Point d'arret
+CODE_POINT = None
 
 # ------------------------------------------------------------
 # POINT D'ENTREE
